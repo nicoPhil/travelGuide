e32ebc2da5262d9156c6125f838c041b4c43d98c
Nicolas Philippe <nicolas.philippe33@gmail.com>
2013-09-16 16:48:17 +0200

    suite

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+	this.resource('guides');
+	this.resource('guide',{
+        path:':guide_id'
+    });
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+App.GuidesRoute = Ember.Route.extend({
+	model: function() {
+		return guides;
+	}
 });
+
+App.GuideRoute = Ember.Route.extend({
+    model: function(params) {
+        return guides.findBy('id', params.guide_id);
+    }
+});
+
+
+var guides = [{
+			id:'1',
+			title: 'Chine',
+			startDate:'10 oct',
+			endDate:'25 oct'
+		}, {
+			id:'2',
+			title: 'Perou',
+			startDate:'22 fev',
+			endDate:'10 juin'
+		}];
